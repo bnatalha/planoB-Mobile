96f8372dfab5bb0ff941bc7084795e1c33f2c41a
natalia <natalia_eletro@hotmail.com>
2020-09-20 23:11:43 -0300

    Show account Info on Transaction Detail

--- lib/app/modules/transactions/pages/transaction_details/transaction_details_page.dart
+++ lib/app/modules/transactions/pages/transaction_details/transaction_details_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:intl/intl.dart';
 
 import '../../../../shared/models/account_model.dart';
 import 'transaction_details_controller.dart';
@@ -30,7 +31,8 @@ class _TransactionDetailsPageState
               children: <Widget>[
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Text('Realizada em ${controller.transaction.date}',
+                  child: Text(
+                      'Realizada em ${DateFormat('yMd').format(controller.transaction.date)}',
                       style: Theme.of(context).textTheme.subtitle1),
                 ),
                 Row(
@@ -47,28 +49,31 @@ class _TransactionDetailsPageState
                   ],
                 ),
                 SizedBox(height: 20),
-                buildaAccDescriptionRow(
-                    context, "From", controller.transaction.source),
+                buildaAccDescription(
+                    context, "Origem", controller.transaction.source),
                 // style: Theme.of(context).textTheme.headline6),
-                buildaAccDescriptionRow(
-                    context, "To", controller.transaction.destination),
+                buildaAccDescription(
+                    context, "Destino", controller.transaction.destination),
                 SizedBox(height: 20),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Row(
                     children: [
-                      SizedBox(width: 20),
-                      IconButton(
-                          icon: Icon(
-                            Icons.edit,
-                            size: 40,
-                          ),
-                          onPressed: () {}),
+                      SizedBox(width: 10),
+                      buildOutlineButton(
+                        color: Colors.amber[200],
+                        iconData: Icons.edit,
+                        title: 'Editar',
+                        onPress: () {},
+                      ),
                       Spacer(),
-                      IconButton(
-                          icon: Icon(Icons.remove_circle, size: 40),
-                          onPressed: () {}),
-                      SizedBox(width: 20),
+                      buildOutlineButton(
+                        color: Colors.red,
+                        iconData: Icons.remove_circle_outline,
+                        title: 'Remover',
+                        onPress: () {},
+                      ),
+                      SizedBox(width: 10),
                     ],
                   ),
                 )
@@ -80,34 +85,81 @@ class _TransactionDetailsPageState
     );
   }
 
-  Row buildaAccDescriptionRow(
+  OutlineButton buildOutlineButton(
+      {Color color, IconData iconData, String title, Function onPress}) {
+    return OutlineButton(
+        child: Row(
+          children: [
+            Icon(
+              iconData,
+              size: 40,
+              color: color,
+            ),
+            SizedBox(width: 10),
+            Text(title, style: Theme.of(context).textTheme.subtitle1,),
+          ],
+        ),
+        onPressed: onPress);
+  }
+
+  // TODO show account balance
+  Widget buildaAccDescription(
       BuildContext context, String lead, AccountModel acc) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          '$lead: ',
-          style: Theme.of(context).textTheme.headline6,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 4,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '$lead',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              Divider(),
+              // SizedBox(width: 5),
+              Row(
+                children: [
+                  Icon(
+                    Icons.person,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(width: 10),
+                  Text('Usuario: ',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText2
+                          .copyWith(fontSize: 16, color: Colors.grey)),
+                  Spacer(),
+                  Text('${acc.user.name}',
+                      style: Theme.of(context).textTheme.headline6),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Icon(
+                    Icons.local_atm,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(width: 10),
+                  Text('Banco: ',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText2
+                          .copyWith(fontSize: 16, color: Colors.grey)),
+                  Spacer(),
+                  Text('${acc.name}',
+                      style: Theme.of(context).textTheme.headline6),
+                ],
+              ),
+            ],
+          ),
         ),
-        SizedBox(width: 5),
-        Text(
-          '${acc.user.name}',
-          style: Theme.of(context)
-              .textTheme
-              .headline6
-              .copyWith(color: Colors.lightBlueAccent),
-        ),
-        SizedBox(width: 5),
-        Text('at', style: Theme.of(context).textTheme.headline6),
-        SizedBox(width: 5),
-        Text(
-          '${acc.name}',
-          style: Theme.of(context)
-              .textTheme
-              .headline6
-              .copyWith(color: Colors.blue),
-        ),
-      ],
+      ),
     );
   }
 }
